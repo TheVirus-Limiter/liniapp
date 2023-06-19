a3c793595f3ea3cd169600291b7c9f32ec4fe9cf
TheVirus-Limiter <64677380+TheVirus-Limiter@users.noreply.github.com>
2023-06-19 17:55:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width">
+      <title>Lini - Where Lines Disappear</title>
+      <link href="style.css" rel="stylesheet" type="text/css" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+      <link rel="icon" href="https://media.discordapp.net/attachments/796586867457130496/1120400504221085696/imageedit_3_2639138992.png">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="script.js"></script>
+  </head>
+    <body>
+	  
+        <div class="container">
+	  
+		<!--────────────────Header───────────────-->
+	<header>
+		<a class="logo" href="https://www.gofundme.com/f/lini-trade-spots-in-line" target="_blank">
+              <img src="https://media.discordapp.net/attachments/796586867457130496/1120484228195418192/image-removebg-preview.png?width=884&height=862" alt="logo"/>
+		</a>
+		<nav>	
+		 <ul class="nav-bar"><div class="bg"></div>
+			<li><a class="nav-link active" href="#home">Home</a></li>
+			<li><a class="nav-link" href="https://www.gofundme.com/f/lini-trade-spots-in-line" target="_blank">Donate</a></li>
+			<li><a class="nav-link" href="mailto:theliniapp@gmail.com">Contact</a></li>
+		 </ul>
+			
+			<div class="hamburger">
+				<div class="line1"></div>
+            <div class="line2"></div>
+				<div class="line3"></div>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<!--─────────────────Home────────────────-->
+	    <div id="home">
+		 <div class="filter"></div>
+		 <section class="intro">
+		  <h3>Where lines disappear.<hrr></h3>
+		  <p>Unlock your time with Lini, the revolutionary line management solution.</p>
+		  <p> </p>
+          <p> </p>
+		  <br>
+		  <br>
+		  <a class="nav-blink active" href="https://www.gofundme.com/f/lini-trade-spots-in-line" target="_blank">Donate Now</a>			  
+		  <!--────social media links─────-->
+			 
+
+			 
+		 </section> 
+	  </div>  
+<img src="https://media.discordapp.net/attachments/796586867457130496/1120380511794253844/ue809.jpg?width=1908&height=544">
+<img src="https://media.discordapp.net/attachments/796586867457130496/1120382850341019800/7ygai2w.jpg?width=1907&height=555">
+<img src="https://media.discordapp.net/attachments/796586867457130496/1120384974672109628/1827hhw.jpg?width=1908&height=584">
+<div class="img-holder">
+<img src="https://media.discordapp.net/attachments/796586867457130496/1120387002102202448/821ayh.jpg?width=1906&height=645">
+<a class="link active" href="https://www.gofundme.com/f/lini-trade-spots-in-line" target="_blank" style="margin-bottom: 80px; margin-right: 885px; scale: 1.29;">Donate Now!</a>
+</div>
+<img src="https://media.discordapp.net/attachments/796586867457130496/1120393657133051936/178A2HA.jpg?width=1908&height=584">
+<div class="img-holder">
+<img src="https://media.discordapp.net/attachments/796586867457130496/1120395224536068156/u8a9021j.jpg?width=1906&height=916">
+<a class="link active" href="mailto:theliniapp@gmail.com" style="margin-bottom: 520px; margin-right: 700px; scale: 1;">theliniapp@gmail.com</a>
+</div>
+  </body>
+</html>
